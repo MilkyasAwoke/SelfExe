418273b6f8e6843893085bc63851db712c0413f9
MilkyasAwoke <milkyas.awoke@tekschool.us>
2024-02-01 19:50:40 -0500

    This is my master first commit

---
+++ src/gitExercise/firstGitExe.java
@@ -0,0 +1,12 @@
+package gitExercise;
+
+public class firstGitExe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstGit = "My first git exercise";
+		System.out.println(firstGit);
+
+	}
+
+}
